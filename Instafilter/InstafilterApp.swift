502fb493f94eb58a41e869fc9e5623411ec70aff
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-06 01:59:53 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Miguel Quezada on 06-06-22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
